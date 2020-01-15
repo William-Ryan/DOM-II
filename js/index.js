4c5d69652654b6d4ced8612c6dd0fc316fed520c
William Ryan <billy.ryan.rom@gmail.com>
2020-01-15 05:18:10 -0500

    Added Event to Logo Header

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 // Your code goes here
 const title = document.querySelector(".logo-heading");
 
+title.addEventListener("click", () => {
+    gsap.to(".logo-heading", {
+        duration: 1,
+        rotate: 720,
+        y: 200,
+        yoyo: true,
+        repeat: 1
+    })
+})
